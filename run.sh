e19a9e812042af7a3003787daad5dc8a06c6dc11
Florian Demmer <fdemmer@gmail.com>
2016-03-25 13:56:41 +0100

    Ask for service_name in run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #/bin/bash
 
+until [[ ${service_name} =~ ^[a-z0-9]+$ ]]; do
+	read -p $'Choose a service name. Please use only numbers and letters:\n> ' service_name
+done
+
 # determine script directory and go there
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR}
+
+# set service_name
+sed -i -e 's/service_name = "myproject"/service_name = "'${service_name}'"/g' Vagrantfile
 
 # remove bootstrap git
 rm -rf ./.git
